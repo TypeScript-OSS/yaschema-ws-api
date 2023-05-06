004d6aef798c6179311443086347a34d6c29c3f5
Brian Westphal <brian.westphal@panchaininc.com>
2023-05-06 13:50:03 +0800

    v2.0.0: - Now depends on yaschema-api 3 - `WsApi` now extends `Api` (from yaschema-api) - The new `name` field can be auto-generated when using `makeWsApi`

--- src/type-makers/make-ws-api.ts
+++ src/type-makers/make-ws-api.ts
@@ -2,6 +2,17 @@ import type { AnyCommands } from '../types/AnyCommands';
 import type { AnyQuery } from '../types/AnyQuery';
 import type { WsApi } from '../types/WsApi';
 
+/**
+ * If name is omitted, it will be generated as:
+ * ```
+ * `WS ${api.routeType} ${api.url}`
+ * ```
+ */
 export const makeWsApi = <RequestCommandsT extends AnyCommands, ResponseCommandsT extends AnyCommands, QueryT extends AnyQuery>(
-  api: Omit<WsApi<RequestCommandsT, ResponseCommandsT, QueryT>, 'isYaschemaWsApi'>
-) => ({ ...api, isYaschemaWsApi: true });
+  api: Omit<WsApi<RequestCommandsT, ResponseCommandsT, QueryT>, 'isYaschemaApi' | 'isYaschemaWsApi' | 'name'> & { name?: string }
+): WsApi<RequestCommandsT, ResponseCommandsT, QueryT> => ({
+  ...api,
+  name: api.name ?? `WS ${api.routeType} ${api.url}`,
+  isYaschemaApi: true,
+  isYaschemaWsApi: true
+});
--- src/types/WsApi.ts
+++ src/types/WsApi.ts
@@ -1,14 +1,12 @@
 import type { Schema } from 'yaschema';
+import type { Api } from 'yaschema-api';
 
 import type { AnyCommands } from './AnyCommands';
 import type { AnyQuery } from './AnyQuery';
 
 /** A generic WebSocket API description */
-export interface WsApi<RequestCommandsT extends AnyCommands, ResponseCommandsT extends AnyCommands, QueryT extends AnyQuery> {
+export interface WsApi<RequestCommandsT extends AnyCommands, ResponseCommandsT extends AnyCommands, QueryT extends AnyQuery> extends Api {
   isYaschemaWsApi: true;
-
-  /** The type of route, which will be used to select a URL base and is configurable using `setUrlBaseForRouteType` */
-  routeType: string;
 
   /** The URL for accessing this API, which may be relative to a URL base configuring for the `routeType`.  Use `{…}` syntax to mark
    * parameters, as you might with Express. */
